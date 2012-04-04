73cb4fbc300884c890640f3cacb7d8cfb3a7fd97
Pasquale Lodise <pakilodi@gmail.com>
2012-04-04 22:42:54 +0200

    update MDP header to latest version

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Fri Apr 27 02:56:10 CDT 2012"
-#define LINUX_COMPILE_TIME "02:56:10"
+#define UTS_VERSION "#3 PREEMPT Sat Apr 28 02:07:15 CDT 2012"
+#define LINUX_COMPILE_TIME "02:07:15"
 #define LINUX_COMPILE_BY "thekraven"
 #define LINUX_COMPILE_HOST "icsbox"
 #define LINUX_COMPILE_DOMAIN "(none)"
