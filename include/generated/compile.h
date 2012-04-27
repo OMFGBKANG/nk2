767713751a14e3ac5df3e3707174a552444f524f
thekraven <savethestyle1@gmail.com>
2012-04-27 02:34:48 -0500

    fix 1

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Fri Apr 27 00:07:41 CDT 2012"
+#define LINUX_COMPILE_TIME "00:07:41"
+#define LINUX_COMPILE_BY "thekraven"
+#define LINUX_COMPILE_HOST "icsbox"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
