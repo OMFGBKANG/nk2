60b804a227b1b3e149eacd7a335f96460b0094f5
thekraven <savethestyle1@gmail.com>
2012-04-28 16:01:33 -0500

    erase memory pages when these are empty and freed 443edac7cd0bd03f8b6ec9a9981242e62dd786f5

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Sat Apr 28 09:26:48 CDT 2012"
-#define LINUX_COMPILE_TIME "09:26:48"
+#define UTS_VERSION "#5 PREEMPT Sat Apr 28 10:15:17 CDT 2012"
+#define LINUX_COMPILE_TIME "10:15:17"
 #define LINUX_COMPILE_BY "thekraven"
 #define LINUX_COMPILE_HOST "icsbox"
 #define LINUX_COMPILE_DOMAIN "(none)"
