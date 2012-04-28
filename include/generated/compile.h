948762c220253676a6fc2e3615fe23267ca0f7df
thekraven <savethestyle1@gmail.com>
2012-04-27 22:28:01 -0500

    qtaguid merge1

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Fri Apr 27 00:07:41 CDT 2012"
-#define LINUX_COMPILE_TIME "00:07:41"
+#define UTS_VERSION "#2 PREEMPT Fri Apr 27 02:56:10 CDT 2012"
+#define LINUX_COMPILE_TIME "02:56:10"
 #define LINUX_COMPILE_BY "thekraven"
 #define LINUX_COMPILE_HOST "icsbox"
 #define LINUX_COMPILE_DOMAIN "(none)"
