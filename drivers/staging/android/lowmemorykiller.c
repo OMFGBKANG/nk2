d2d7faf5f02afc79ffda58838e1bc17cb6fc0946
Thạch Tùng Anh <thachtunganh@Gmail.com>
2012-06-10 13:04:18 +0700

    thunderg: minfree tweaks.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -51,12 +51,14 @@ static int lowmem_adj[6] = {
 };
 static int lowmem_adj_size = 4;
 static size_t lowmem_minfree[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	16 * 1024,	/* 64MB */
+	1536,
+	2304,
+	4096,
+	17920,
+	19456,
+	33472
 };
-static int lowmem_minfree_size = 4;
+static int lowmem_minfree_size = 6;
 
 static size_t fork_boost_adj[6] = {
 	0,
@@ -369,8 +371,7 @@ static void __exit lowmem_exit(void)
 module_param_named(cost, lowmem_shrinker.seeks, int, S_IRUGO | S_IWUSR);
 module_param_array_named(adj, lowmem_adj, int, &lowmem_adj_size,
 			 S_IRUGO | S_IWUSR);
-module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
-			 S_IRUGO | S_IWUSR);
+module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size, 00444);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
 module_param_named(fork_boost, lowmem_fork_boost, uint, S_IRUGO | S_IWUSR);
 
