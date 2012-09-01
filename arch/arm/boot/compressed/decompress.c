b1d95b736d5d2bd38923b269f3954a5f21efc819
thekraven <savethestyle1@gmail.com>
2012-09-01 07:14:44 -0500

    remove unneccessary item by rashed97

--- arch/arm/boot/compressed/decompress.c
+++ arch/arm/boot/compressed/decompress.c
@@ -46,5 +46,5 @@ extern void error(char *);
 
 void do_decompress(u8 *input, int len, u8 *output, void (*error)(char *x))
 {
-	return decompress(input, len, NULL, NULL, output, NULL, error);
+	decompress(input, len, NULL, NULL, output, NULL, error);
 }
